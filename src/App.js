6dc4ad618cda4b872663728a3182ed0e260c3deb
Aziat <abdimalikovaziat@gmail.com>
2023-02-14 13:05:27 +0600

    todo with redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Navigate, Route, Routes } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
+import AuthPage from "./page/AuthPage";
+import TodoPage from "./page/TodoPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/login" element={<AuthPage />} />
+        <Route path="/todos" element={<TodoPage />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+      <GlobalStyle />
     </div>
   );
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+    body{
+      background-color: aliceblue;
+    }
+`;
